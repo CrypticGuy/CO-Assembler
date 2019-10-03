c1a216f113e6071a4f4886c0ef73ce744fb55d4e
Vasu Goel <alohanvasu@gmail.com>
2019-10-04 01:27:46 +0530

    A basic version is ready. Some methods need to be declared

--- FirstPass.py
+++ FirstPass.py
@@ -3,20 +3,21 @@ literalTable = {} # dict to implement literals
 macrosTable   = {} # to keep track of macros
 lc = 0 # Location Counter
 
+# assemblyCode : [Opcode, instructionSize]
 opcodeTable = {
-    "CLA": "0000", # Clear Accumulator
-    "LAC": "0001", # Load into AC from Address
-    "SAC": "0010", # Store AC into address
-    "ADD": "0011", # AC <- AC + M[x]
-    "SUB": "0100", # AC <- AC - M[x]
-    "BRZ": "0101", # Branch to address if AC == 0
-    "BRN": "0110", # Branch to address if AC < 0 
-    "BRP": "0111", # Branch to address if AC > 0
-    "INP": "1000", # Take Input from terminal into address
-    "DSP": "1001", # Display value in address
-    "MUL": "1010", # AC <- AC * M[x]
-    "DIV": "1011", # AC / M[x] -> R1 and AC % M[x] -> R2
-    "STP": "1100", # Stop Execution
+    "CLA": ["0000"], # Clear Accumulator
+    "LAC": ["0001"], # Load into AC from Address
+    "SAC": ["0010"], # Store AC into address
+    "ADD": ["0011"], # AC <- AC + M[x]
+    "SUB": ["0100"], # AC <- AC - M[x]
+    "BRZ": ["0101"], # Branch to address if AC == 0
+    "BRN": ["0110"], # Branch to address if AC < 0 
+    "BRP": ["0111"], # Branch to address if AC > 0
+    "INP": ["1000"], # Take Input from terminal into address
+    "DSP": ["1001"], # Display value in address
+    "MUL": ["1010"], # AC <- AC * M[x]
+    "DIV": ["1011"], # AC / M[x] -> R1 and AC % M[x] -> R2
+    "STP": ["1100"], # Stop Execution
 }
 
 with open('input.assembly', 'r') as reader:
@@ -33,6 +34,20 @@ with open('input.assembly', 'r') as reader:
             literal = checkLiteral(line)
             if (literal != False):
                 addLiteral(literal)
+            opcode = getOpcode(parts)
+            # type = search_opcode_table(opcode) -> given in tannenbaum don't know if we need it
+            if (opcode == False):
+                pseudoOpcode = isPseudoOpcode(parts)
+                break
+            else:
+                assemblyCode = 0
+                if (symbol):
+                    assemblyCode = 1
+                instructionSize = opcodeTable[parts[assemblyCode]][1]
+                lc += instructionSize
+            if (opcode == "1100"):
+                # The end of the file
+                removeRedundantLiterals()
         line = reader.readline()
 
 
@@ -63,4 +78,24 @@ def addLiteral(literal):
     return True
 
 def getOpcode(parts):
-    return True
+    i = 1
+    if (parts[0][-1] != ':'):
+        i = 0
+    try:
+        x = opcodeTable[parts[i]]
+        return x
+    except:
+        print("%s is not a valid opcode!", parts[i])
+        return False
+
+def isPseudoOpcode(parts):
+    # Will be used if we add any pseudo literals to our language -> most prob wont be used
+    return False
+
+def handleMacros():
+    # This will be tricky
+    return False
+
+def removeRedundantLiterals():
+    # Go through the literal table and remove extra entries
+    return False
