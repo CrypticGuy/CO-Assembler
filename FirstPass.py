0f0cc8e820d206bbca7f25a4aae82d923f2f5846
Vasu Goel <alohanvasu@gmail.com>
2019-10-04 01:35:47 +0530

    Symbol table is being generated properly as of now, but functions need to include corner cases and report errors properly

--- FirstPass.py
+++ FirstPass.py
@@ -5,58 +5,30 @@ lc = 0 # Location Counter
 
 # assemblyCode : [Opcode, instructionSize]
 opcodeTable = {
-    "CLA": ["0000"], # Clear Accumulator
-    "LAC": ["0001"], # Load into AC from Address
-    "SAC": ["0010"], # Store AC into address
-    "ADD": ["0011"], # AC <- AC + M[x]
-    "SUB": ["0100"], # AC <- AC - M[x]
-    "BRZ": ["0101"], # Branch to address if AC == 0
-    "BRN": ["0110"], # Branch to address if AC < 0 
-    "BRP": ["0111"], # Branch to address if AC > 0
-    "INP": ["1000"], # Take Input from terminal into address
-    "DSP": ["1001"], # Display value in address
-    "MUL": ["1010"], # AC <- AC * M[x]
-    "DIV": ["1011"], # AC / M[x] -> R1 and AC % M[x] -> R2
-    "STP": ["1100"], # Stop Execution
+    "CLA": ["0000", 1], # Clear Accumulator
+    "LAC": ["0001", 1], # Load into AC from Address
+    "SAC": ["0010", 1], # Store AC into address
+    "ADD": ["0011", 1], # AC <- AC + M[x]
+    "SUB": ["0100", 1], # AC <- AC - M[x]
+    "BRZ": ["0101", 1], # Branch to address if AC == 0
+    "BRN": ["0110", 1], # Branch to address if AC < 0 
+    "BRP": ["0111", 1], # Branch to address if AC > 0
+    "INP": ["1000", 1], # Take Input from terminal into address
+    "DSP": ["1001", 1], # Display value in address
+    "MUL": ["1010", 1], # AC <- AC * M[x]
+    "DIV": ["1011", 1], # AC / M[x] -> R1 and AC % M[x] -> R2
+    "STP": ["1100", 1], # Stop Execution
 }
-
-with open('input.assembly', 'r') as reader:
-    line = reader.readline()
-    while line != '':
-        print(line, end='')
-        parts = line.strip().split()
-        print(parts)
-        length = 0
-        if (not comment(line)):
-            symbol = checkSymbol(line)
-            if (symbol != False):
-                addNewSymbol(symbol, lc)
-            literal = checkLiteral(line)
-            if (literal != False):
-                addLiteral(literal)
-            opcode = getOpcode(parts)
-            # type = search_opcode_table(opcode) -> given in tannenbaum don't know if we need it
-            if (opcode == False):
-                pseudoOpcode = isPseudoOpcode(parts)
-                break
-            else:
-                assemblyCode = 0
-                if (symbol):
-                    assemblyCode = 1
-                instructionSize = opcodeTable[parts[assemblyCode]][1]
-                lc += instructionSize
-            if (opcode == "1100"):
-                # The end of the file
-                removeRedundantLiterals()
-        line = reader.readline()
 
 
 def comment(line):
-    if (line.strip()[0] == "#"):
+    if (line != "\n" and line.strip()[0] == "#"):
         return True
     return False
 
 def checkSymbol(line):
+    if (line == "\n"):
+        return False
     couldBeSymbol = line.strip().split(' ')[0]
     if (couldBeSymbol[-1] == ':'):
         return couldBeSymbol
@@ -69,7 +41,7 @@ def addNewSymbol(symbol, lc):
         print("Duplicate Symbol")
     symbolTable[symbol] = lc
 
-def checkLiteral():
+def checkLiteral(line):
     # Need to be implemented
     return False
 
@@ -79,6 +51,8 @@ def addLiteral(literal):
 
 def getOpcode(parts):
     i = 1
+    if (len(parts) <= 0):
+        return False
     if (parts[0][-1] != ':'):
         i = 0
     try:
@@ -99,3 +73,36 @@ def handleMacros():
 def removeRedundantLiterals():
     # Go through the literal table and remove extra entries
     return False
+
+with open('input.assembly', 'r') as reader:
+    line = reader.readline()
+    while line != '':
+        #print(line, end='')
+        parts = line.strip().split()
+        #print(parts)
+        length = 0
+        if (not comment(line)):
+            symbol = checkSymbol(line)
+            if (symbol != False):
+                addNewSymbol(symbol, lc)
+            literal = checkLiteral(line)
+            if (literal != False):
+                addLiteral(literal)
+            opcode = getOpcode(parts)
+            # type = search_opcode_table(opcode) -> given in tannenbaum don't know if we need it
+            if (opcode == False):
+                pseudoOpcode = isPseudoOpcode(parts)
+                break
+            else:
+                assemblyCode = 0
+                if (symbol):
+                    assemblyCode = 1
+                instructionSize = opcodeTable[parts[assemblyCode]][1]
+                lc += instructionSize
+            if (opcode[0] == "1100"):
+                # The end of the file
+                removeRedundantLiterals()
+            print(opcode)
+        line = reader.readline()
+
+print(symbolTable)
